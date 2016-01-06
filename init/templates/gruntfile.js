6a630b9e7ca34351b84e5ac6650fa146c87b53c6
John Gully <john@gully.us>
2016-01-06 15:31:24 -0600

    Added more granular grunt release tasks

--- init/templates/gruntfile.js
+++ init/templates/gruntfile.js
@@ -3,12 +3,12 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     patternpack: {
-      options: {
-        assets: "./src/assets"
-      },
       run: {},
       build: {},
-      release: {}
+      release: {},
+      "release-major": {},
+      "release-minor": {},
+      "release-patch": {}
     }
   });
 
@@ -17,5 +17,8 @@ module.exports = function (grunt) {
   grunt.registerTask("run", ["patternpack:run"]);
   grunt.registerTask("build", ["patternpack:build"]);
   grunt.registerTask("release", ["patternpack:release"]);
+  grunt.registerTask("release-major", ["patternpack:release-major"]);
+  grunt.registerTask("release-minor", ["patternpack:release-minor"]);
+  grunt.registerTask("release-patch", ["patternpack:release-patch"]);
   grunt.registerTask("default", ["run"]);
 };
